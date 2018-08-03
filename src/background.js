1a0472d7fd48116c5ca30ce302d1db07c3ba564f
Michiel de Jong <michiel@unhosted.org>
2018-08-03 10:54:25 +0200

    switch to AirBnb style guide

--- src/background.js
+++ src/background.js
@@ -1,64 +1,65 @@
+/* global browser, getServices, getService, getIconForService */
 /* requires ./popup/js/servicedata.js */
-
-getServices().then((services) => {
-  browser.storage.local.set(services).then(() => {
-    /* When first loaded, initialize the page action for all tabs.
-    */
-    var gettingAllTabs = browser.tabs.query({})
-    return gettingAllTabs.then((tabs) => {
-      for (let tab of tabs) {
-        // Only active tabs should get the pageAction
-        if (tab.active) {
-          initializePageAction(tab)
-        }
-      }
-    })
-  })
-})
 
 /*
 Initialize the page action: set icon and title, then show.
 Only operates on tabs whose URL's protocol is applicable.
 */
-function initializePageAction (tab) {
+function initializePageAction(tab) {
   // console.log('initializePageAction', tab);
   return getService(tab).then((service) => {
-    console.log('got service', service)
+    // console.log('got service', service);
     if (service) {
       browser.pageAction.setIcon({
         tabId: tab.id,
-        path: getIconForService(service)
-      })
+        path: getIconForService(service),
+      });
       browser.pageAction.setPopup({
         tabId: tab.id,
-        popup: 'popup/popup.html#' + service.mainDomain
-      })
-      browser.pageAction.show(tab.id)
-      checkNotification(service)
+        popup: `popup/popup.html#${service.mainDomain}`,
+      });
+      browser.pageAction.show(tab.id);
+      checkNotification(service);
     } else {
       browser.pageAction.setIcon({
         tabId: tab.id,
-        path: 'icons/class/none.png'
-      })
+        path: 'icons/class/none.png',
+      });
       browser.pageAction.setPopup({
         tabId: tab.id,
-        popup: 'popup/popup.html#none'
-      })
-      browser.pageAction.show(tab.id)
+        popup: 'popup/popup.html#none',
+      });
+      browser.pageAction.show(tab.id);
     }
-  }).catch(err => {
+  }).catch((err) => {
     if (err.message === 'no domain name provided') {
-      return
+      return;
     }
-    console.error(err)
-  })
+    console.error(err); // eslint-disable-line no-console
+  });
 }
+
+getServices().then((services) => {
+  browser.storage.local.set(services).then(() => {
+    /* When first loaded, initialize the page action for all tabs.
+    */
+    const gettingAllTabs = browser.tabs.query({});
+    return gettingAllTabs.then((tabs) => {
+      tabs.forEach((t) => {
+        // Only active tabs should get the pageAction
+        if (t.active) {
+          initializePageAction(t);
+        }
+      });
+    });
+  });
+});
 
 // Run when loading the tab completes
 // console.log('setting tab event listeners');
 browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
   // console.log('updated', id, changeInfo, tab);
   if (changeInfo.status === 'complete') {
-    initializePageAction(tab)
+    initializePageAction(tab);
   }
-})
+});
--- src/popup/js/servicedata.js
+++ src/popup/js/servicedata.js
@@ -1,45 +1,47 @@
-const APPLICABLE_PROTOCOLS = ['http:', 'https:']
-const REVIEW_PREFIX = 'tosdr/review/'
-const DEBUG = false
-const RATING_TEXT = {
-  'D': 'The terms of service are very uneven or there are some important issues that need your attention.',
-  'E': 'The terms of service raise very serious concerns.'
-}
+/* globals document, browser: true, chrome, fetch, Request */
 
-var services = []
+const APPLICABLE_PROTOCOLS = ['http:', 'https:']; // eslint-disable-line no-unused-vars
+const REVIEW_PREFIX = 'tosdr/review/'; // eslint-disable-line no-unused-vars
+const DEBUG = false;
+const RATING_TEXT = { // eslint-disable-line no-unused-vars
+  D: 'The terms of service are very uneven or there are some important issues that need your attention.',
+  E: 'The terms of service raise very serious concerns.',
+};
 
-/** ***************** UTILS ****************/
-function log (message) {
+const services = []; // eslint-disable-line no-unused-vars
+
+/** ***************** UTILS *************** */
+function log(message) { // eslint-disable-line no-unused-vars
   if (DEBUG) {
-    console.log(message)
+    console.log(message); // eslint-disable-line no-console
   }
 }
 
 /*
 Returns true only if the URL's protocol is in APPLICABLE_PROTOCOLS.
 */
-function getDomain (url) {
-  var anchor = document.createElement('a')
-  anchor.href = url
+function getDomain(url) {
+  const anchor = document.createElement('a');
+  anchor.href = url;
   if (APPLICABLE_PROTOCOLS.includes(anchor.protocol)) {
-    return anchor.hostname
+    return anchor.hostname;
   }
+  return null;
 }
 
-function getServices () {
-  const requestURL = 'https://tosdr.org/api/1/all.json'
+function getServices() { // eslint-disable-line no-unused-vars
+  const requestURL = 'https://tosdr.org/api/1/all.json';
 
   const driveRequest = new Request(requestURL, {
-    method: 'GET'
-  })
+    method: 'GET',
+  });
 
   return fetch(driveRequest).then((response) => {
     if (response.status === 200) {
-      return response.json()
-    } else {
-      throw response.status
+      return response.json();
     }
-  })
+    throw response.status;
+  });
 }
 
 // In Firefox, we can access browsers.tabs and browser.storage.
@@ -50,45 +52,43 @@ if (typeof browser === 'undefined') {
   browser = {
     tabs: {
       onUpdated: chrome.tabs.onUpdated,
-      query: (options) => new Promise(resolve => chrome.tabs.query(options, resolve)),
-      create: chrome.tabs.create
+      query: options => new Promise(resolve => chrome.tabs.query(options, resolve)),
+      create: chrome.tabs.create,
     },
     notifications: chrome.notifications,
     storage: {
       local: {
-        get: (keys) => new Promise(resolve => chrome.storage.local.get(keys, resolve)),
-        set: (values) => new Promise(resolve => chrome.storage.local.set(values, resolve))
-      }
+        get: keys => new Promise(resolve => chrome.storage.local.get(keys, resolve)),
+        set: values => new Promise(resolve => chrome.storage.local.set(values, resolve)),
+      },
     },
-    pageAction: chrome.pageAction
-  }
+    pageAction: chrome.pageAction,
+  };
 }
 
-function getDomainEntryFromStorage (domain) {
+function getDomainEntryFromStorage(domain) {
   // console.log('getDomainEntryFromStorage', domain)
-  return browser.storage.local.get(REVIEW_PREFIX + domain).then(resultSet => {
-    return resultSet[REVIEW_PREFIX + domain] || undefined
-  })
+  return browser.storage.local.get(REVIEW_PREFIX + domain)
+    .then(resultSet => resultSet[REVIEW_PREFIX + domain] || undefined);
 }
 
-function getServiceDetails (domain, tries = 0) {
+function getServiceDetails(domain, tries = 0) {
   // console.log('getServiceDetails', domain, tries)
   if (!domain) {
-    return Promise.reject(new Error('no domain name provided'))
+    return Promise.reject(new Error('no domain name provided'));
   }
   if (tries > 10) {
-    return Promise.reject(new Error('too many redirections ' + domain))
+    return Promise.reject(new Error(`too many redirections ${domain}`));
   }
   return getDomainEntryFromStorage(domain).then((details) => {
     // console.log('details', details)
     if (!details) {
-      var domainParts = domain.split('.')
+      const domainParts = domain.split('.');
       if (domainParts.length > 2) {
         // console.log('trying parent domain')
-        return getServiceDetails(domainParts.slice(1).join('.'), tries + 1)
-      } else {
-        return Promise.reject(new Error('details not found'))
+        return getServiceDetails(domainParts.slice(1).join('.'), tries + 1);
       }
+      return Promise.reject(new Error('details not found'));
     }
     if (details.see) {
       // console.log('see', details.see)
@@ -104,22 +104,22 @@ function getServiceDetails (domain, tries = 0) {
       // >   ... details you want
       // > }
       // > ...
-      return getServiceDetails(details.see, tries + 1)
+      return getServiceDetails(details.see, tries + 1);
     }
-    details.mainDomain = domain // used as storage key when marking that notification has been displayed
     // console.log('mainDomain set', details)
-    return details
-  })
+    return Object.assign({
+      mainDomain: domain, // used as storage key when marking that notification has been displayed
+    }, details);
+  });
 }
 
-function getService (tab) {
+function getService(tab) { // eslint-disable-line no-unused-vars
   // console.log('getService', tab)
-  var domain = getDomain(tab.url)
-  return getServiceDetails(domain)
+  const domain = getDomain(tab.url);
+  return getServiceDetails(domain);
 }
 
-function getIconForService (service) {
-  var rated = service.rated
-  var imageName = rated ? rated.toLowerCase() : 'false'
-  return 'icons/class/' + imageName + '.png'
+function getIconForService(service) { // eslint-disable-line no-unused-vars
+  const imageName = service.rated ? service.rated.toLowerCase() : 'false';
+  return `icons/class/${imageName}.png`;
 }

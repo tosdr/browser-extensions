8c332bd2654df6f44cb0c4d571bab8115567f1dd
Razvan Caliman <razvan.caliman+dev@gmail.com>
2019-06-10 12:15:55 +0200

    refactor popup to use vanilla JS instead of jQuery
    Changes:
    - restructures popup.js to split code into meaningful parts;
    - uses vanilla JS instead of jQuery for DOM mutations;
    - uses `<template>` HTML elements to group markup and uses `document.importNode()` to make use and populate with data before appending to the DOM;
    - replaces `target="_blank"` with `<base>` tag to ensure all links open in new tab by default;
    - removes unused `class` and `id` attributes from markup;
    - toggles loading state based on `aria-busy` attribute and ensures the loading state is always removed even after errors are thrown;
    - uses `RATING_TEXT` from `constants.js` shared between `servicedata.js` and `backgrond.js`;

--- src/popup/js/popup.js
+++ src/popup/js/popup.js
@@ -1,4 +1,4 @@
-/* global window, $, jQuery, getServiceDetails */
+/* global window, getServiceDetails, RATING_TEXT */
 
 function escapeHTML(unsafe) {
   return (`${unsafe}`)
@@ -9,122 +9,197 @@ function escapeHTML(unsafe) {
     .replace(/'(?!#039;)/g, '&#039;');
 }
 
-jQuery(() => {
-  function tosdrPoint(serviceName, dataPoint) {
-    let badge;
-    let icon;
-    // let sign;
-    if (dataPoint) {
-      if (dataPoint.point === 'good') {
-        badge = 'badge-success';
-        icon = 'thumbs-up';
-        // sign = '+';
-      } else if (dataPoint.point === 'bad') {
-        badge = 'badge-warning';
-        icon = 'thumbs-down';
-        // sign = '-';
-      } else if (dataPoint.point === 'blocker') {
-        badge = 'badge-important';
-        icon = 'remove';
-        // sign = '×';
-      } else if (dataPoint.point === 'neutral') {
-        badge = 'badge-neutral';
-        icon = 'asterisk';
-        // sign = '→';
-      } else {
-        badge = '';
-        icon = 'question-sign';
-        // sign = '?';
-      }
-      const pointText = dataPoint.description || '';
+/*
+  Return a DocumentFragment with the DOM structure for a ToS data point
+  with badge, summary and description to be appended to the list of points.
+  See <template id="point-template"> for markup.
 
-      // Extract links from text
-      const taggedText = pointText.split(/(<\/?\w+(?:(?:\s+\w+(?:\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)\/?>)/gim);
-      $(`#popup-point-${serviceName}-${dataPoint.id}`)
-        .append($('<div>', { class: dataPoint.point })
-          .append($('<h5>')
-            .append($('<span>', { class: `badge ${badge}`, title: escapeHTML(dataPoint.point) })
-              .append($('<span>', { class: `glyphicon glyphicon-${icon}` })))
-            .append($('<span>').text(` ${dataPoint.title} `))
-            .append($('<a>', {
-              href: escapeHTML(dataPoint.discussion), target: '_blank', class: 'label context', text: 'Discussion',
-            }))));
+  @param {Object} dataPoint
+         An object with data about a ToS point. Example structure:
+         {
+           id:          {String}
+           title:       {String} // Summary of ToS point
+           description: {String} // Description of ToS point
+           discussion	  {String} // URL to discussion about ToS point
+           point	      {String} // One of: "good", "bad", "blocker", "neutral"
+           score	      {Number}
+         }
 
-      $(`#popup-point-${serviceName}-${dataPoint.id}`).append($('<p>'));
-      if (taggedText.length > 1) {
-        taggedText.forEach((t) => {
-          $(`#popup-point-${serviceName}-${dataPoint.id} p`).append(t);
-        });
-      } else {
-        $(`#popup-point-${serviceName}-${dataPoint.id} p`).text(pointText);
-      }
-    }
+  @returns {DocumentFragment}
+*/
+function renderPoint(dataPoint) {
+  let badge;
+  let icon;
+  switch (dataPoint.point) {
+    case 'good':
+      badge = 'badge-success';
+      icon = 'thumbs-up';
+      break;
+    case 'bad':
+      badge = 'badge-warning';
+      icon = 'thumbs-down';
+      break;
+    case 'blocker':
+      badge = 'badge-important';
+      icon = 'remove';
+      break;
+    case 'neutral':
+      badge = 'badge-neutral';
+      icon = 'asterisk';
+      break;
+    default:
+      badge = '';
+      icon = 'question-sign';
   }
 
-  const NOT_RATED_TEXT = "We haven't sufficiently reviewed the terms yet. Please contribute to our group: tosdr@googlegroups.com.";
-  const RATING_TEXT = {
-    0: NOT_RATED_TEXT,
-    false: NOT_RATED_TEXT,
-    A: 'The terms of service treat you fairly, respect your rights and follows the best practices.',
-    B: 'The terms of services are fair towards the user but they could be improved.',
-    C: 'The terms of service are okay but some issues need your consideration.',
-    D: 'The terms of service are very uneven or there are some important issues that need your attention.',
-    E: 'The terms of service raise very serious concerns.',
-  };
+  const pointText = dataPoint.description || '';
+  // Extract links from text
+  let taggedText = pointText.split(/(<\/?\w+(?:(?:\s+\w+(?:\s*=\s*(?:".*?"|'.*?'|[^'">\s]+))?)+\s*|\s*)\/?>)/gim);
 
+  const template = document.getElementById('point-template');
+  // Create a DocumentFragment from the template element.
+  const node = document.importNode(template.content, true);
+
+  const badgeEl = node.querySelector("[js-badge]");
+  const badgeIconEl = node.querySelector("[js-badge-icon]");
+  const titleEl = node.querySelector("[js-title]");
+  const discussionLinkEl = node.querySelector("[js-discussion-link]");
+  const descriptionEl = node.querySelector("[js-description]");
+
+  badgeEl.classList.add(badge);
+  badgeEl.title = escapeHTML(dataPoint.point);
+  badgeIconEl.classList.add(`glyphicon-${icon}`);
+  titleEl.textContent = dataPoint.title;
+  discussionLinkEl.href = escapeHTML(dataPoint.discussion);
+  // Ensure taggedText is an array.
+  taggedText = Array.isArray(taggedText) ? taggedText : [taggedText];
+  for (text of taggedText) {
+    descriptionEl.append(text);
+  }
+
+  return node;
+}
+
+/*
+  Return a DocumentFragment with the content for the list of links to
+  additional reading material, such as terms of service, privacy policy, etc.
+
+  @param {Array} documents
+          List of objects describing additional reading materials.
+          Example structure:
+          [
+            {
+              href: {String} // URL to document,
+              name: {String} // document title
+            }
+          ]
+
+  @returns {DocumentFragment}
+*/
+function renderDocuments(documents = []) {
+  // Return an empty string when there are no documents.
+  // This way, Node.append() using this method's return value won't throw.
+  if (!documents.length) {
+    return "";
+  }
+
+  const template = document.getElementById('documents-template');
+  // Create a DocumentFragment from the template element.
+  const node = document.importNode(template.content, true);
+  const list = node.querySelector("[js-list]");
+
+  for (doc of documents) {
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = escapeHTML(doc.url);
+    link.textContent = doc.name;
+
+    li.append(link)
+    list.append(li);
+  }
+
+  return node;
+}
+
+/*
+  Return a DocumentFragment with the section content for the rating
+  label and rating overview for a given rating label.
+
+  @param   {String|Boolean} rated
+           Rating label may be either a string or boolean `false`.
+           See keys of RATING_TEXT for rating labels.
+
+  @returns {DocumentFragment}
+*/
+function renderRatingOverview(rated) {
+  const template = document.getElementById('rating-template');
+  const node = document.importNode(template.content, true);
+
+  const ratingLabelEl = node.querySelector("[js-rating-label");
+  const ratingTextEl = node.querySelector("[js-rating-text");
+
+  ratingLabelEl.classList.add(rated);
+  ratingLabelEl.textContent = rated ? `Class ${rated}` : 'No Class Yet';
+  ratingTextEl.textContent = RATING_TEXT[rated];
+
+  return node;
+}
+
+/*
+  Return a DocumentFragment with the content for rating label and text
+  for unknown services.
+*/
+function renderUnknownRating() {
+  const template = document.getElementById('rating-unknown-template');
+  const node = document.importNode(template.content, true);
+
+  return node;
+}
+
+function render() {
+  // Pluck the service URL from the current window's hash.
+  // The background page invokes popup.html with the hash assigned.
   const serviceUrl = window.location.hash.substr(1);
-  function updatePopup() {
-    $('.loading').show();
+  const serviceLink = document.querySelector('#service_url');
+  const pointsList = document.querySelector('.tosdr-points');
+  const documentsSection = document.querySelector('#documents');
+  const ratingSection = document.querySelector('.tosdr-rating')
+  const closeButton = document.querySelector('#closeButton');
+  closeButton.addEventListener("click",
+    (e) => { window.close() }, { once: true });
 
-    getServiceDetails(serviceUrl).then((service) => {
+  // Set the document in the "loading" state.
+  // This satisfies a CSS selector that shows the loading indicator for sighted users and announces this state for users of screen readers.
+  document.body.setAttribute("aria-busy", "true");
+
+  getServiceDetails(serviceUrl)
+    .then((service) => {
       if (serviceUrl === 'none') {
-        $('#page').empty();
-        $('#page').append($('<div>', { class: 'modal-body' })
-          .append($('<div>', { class: 'tosdr-rating' })
-            .append($('<h4>', { text: 'Not rated, yet.' }))
-            .append($('<p>', { text: 'Go to https://edit.tosdr.org to help us review it!', class: 'lbldesc' }))));
+        ratingSection.append(renderUnknownRating());
       } else {
-        $('#service_url').attr('href', `http://tosdr.org/#${escapeHTML(service.url)}`);
+        // Link to service details on tosdr.org
+        serviceLink.setAttribute("href", `http://tosdr.org/#${escapeHTML(service.slug)}`)
 
-        // Update class
-        $('#service_class').addClass(service.rated);
-        if (service.rated) {
-          $('#service_class').text(`Class ${service.rated}`);
-          $('#ratingText').text(RATING_TEXT[service.rated]);
-        } else {
-          $('#service_class').text('No Class Yet');
-          $('#service_class').remove();
-          $('#ratingText').text(RATING_TEXT[service.rated]);
-        }
+        // Rating label and overview
+        ratingSection.append(renderRatingOverview(service.rated));
 
         // Points
-        service.points.forEach((p) => {
-          $('.tosdr-points').append($('<li>', { id: `popup-point-${service.name}-${p.id}`, class: 'point' }));
-          tosdrPoint(service.name, p);
-        });
-
-        // links inside of the dataPoints should open in a new window
-        $('.tosdr-points a').attr('target', '_blank');
-
-        if (service.documents.length > 0) {
-          $('#linksList')
-            .append($('<h4>', { text: 'Read the Terms' }))
-            .append($('<ul>', { class: 'tosback2' }));
-
-          service.documents.forEach((d) => {
-            $('.tosback2').append($('<li>')
-              .append($('<a>', { href: escapeHTML(d.url), target: '_blank', text: d.name })));
-          });
+        for (const point of service.points) {
+          pointsList.append(renderPoint(point));
         }
+
+        // Documents
+        documentsSection.append(renderDocuments(service.documents));
       }
-      // [x] Button
-      $('#closeButton,.close').click(() => {
-        window.close();
-      });
-
-      $('.loading').hide();
+    })
+    .catch(err => {
+      console.error(err)
+    })
+    .finally(() => {
+      // Ensure document does not remain in the "loading" state
+      // even if an error is thrown and caught by catch()
+      document.body.removeAttribute("aria-busy");
     });
-  }
+}
 
-  updatePopup();
-});
+document.addEventListener("DOMContentLoaded", render);
